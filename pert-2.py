6a6a2141c8bb5e1ac62428464dbce8b934f5f7ed
Safira Wulandari <68987689+safirawln@users.noreply.github.com>
2022-09-21 12:12:51 +0700

    Add files via upload

---
+++ pert-2.py
@@ -0,0 +1,7 @@
+def login():
+    username = input('Silahkan Masukkan Username Anda : ')
+    password = input('Silahkan Masukkan Password Anda : ')
+    if(username == 'Safira' and password == 'safiraw'):
+        print('Login Berhasil')
+    else:
+        print('Login Gagal')
